64e6ac49228bf4f7f7784d368858a11df50afe38
unknown <jreidmke@gmail.com>
2020-09-11 09:13:41 -0500

    added user reg and login tests

--- test_user_views.py
+++ test_user_views.py
@@ -33,7 +33,7 @@ db.create_all()
 app.config['WTF_CSRF_ENABLED'] = False
 
 
-class MessageViewTestCase(TestCase):
+class UserViewTestCase(TestCase):
     """Test views for messages."""
 
     def setUp(self):
@@ -44,14 +44,39 @@ class MessageViewTestCase(TestCase):
 
         self.client = app.test_client()
 
-        self.testuser = User.signup(username="testuser",
-                                    email="test@test.com",
-                                    password="testuser",
-                                    bio='Hello',
-                                    location='Mexico',
-                                    image_url=None)
+        self.user = User(
+            username="testuser",
+            email="test@test.com",
+            password="password",
+            location='Milwaukee, WI',
+            image_url=None,
+            bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        )
+
+        self.data = {
+            'username': self.user.username,
+            'email': self.user.email,
+            'password': self.user.password,
+            'location': self.user.location,
+            'image_url': self.user.image_url,
+            'bio': self.user.bio
+        }
 
         db.session.commit()
 
     def tearDown(self):
         db.session.rollback()
+
+    def test_user_registration_view(self):
+        resp = self.client.post('/signup', data=self.data, follow_redirects=True)
+        html = resp.get_data(as_text=True)
+        self.assertIn(self.user.username, html)
+
+    def test_user_authentication_views(self):
+        data = {
+            'username': self.user.username,
+            'password': self.user.password
+        }
+        resp = self.client.post('/login', data = data, follow_redirects=True)
+        html = resp.get_data(as_text=True)
+        self.assertIn(self.user.username, html)
